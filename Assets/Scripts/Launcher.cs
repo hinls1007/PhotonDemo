aea7f1f6ea7319c7da84efd45d40cb9f06220dc2
Caden Tang <caden.ks.tang@gtomato.com>
2024-01-09 18:19:16 +0800

    Feat: press Q and E to rotate object

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -17,6 +17,8 @@ public class Launcher : MonoBehaviourPunCallbacks
     public GameObject playerObj;
 
     private string PlayerLocation = "Player_Location";
+    private string PlayerRotation = "Player_Rotation";
+
 
     private void Start()
     {
@@ -35,15 +37,31 @@ public class Launcher : MonoBehaviourPunCallbacks
             var newLocation = new Vector3(x, 0, z);
 
 
+            float rotationInput = 0f;
+            if (Input.GetKey(KeyCode.Q))
+            {
+                rotationInput = 1f;
+            }
+
+            if (Input.GetKey(KeyCode.E))
+            {
+                rotationInput = -1f;
+            }
+
+            Quaternion newRotation = Quaternion.Euler(0f, rotationInput * 10f * Time.deltaTime, 0f);
+
+
             var localPlayer = playerData[PhotonNetwork.LocalPlayer.UserId];
             localPlayer.transform.Translate(newLocation);
+            localPlayer.transform.Rotate(newRotation.eulerAngles);
 
             var properties = PhotonNetwork.LocalPlayer.CustomProperties;
             properties[PlayerLocation] = localPlayer.transform.position;
+            properties[PlayerRotation] = localPlayer.transform.rotation;
 
             PhotonNetwork.LocalPlayer.SetCustomProperties(properties);
 
-            Debug.Log($"Player:{PhotonNetwork.LocalPlayer.UserId}, location:{localPlayer.transform.position}, properties:{properties[PlayerLocation]}");
+            Debug.Log($"Player:{PhotonNetwork.LocalPlayer.UserId}, location:{localPlayer.transform.position}, rotation:{localPlayer.transform.rotation} properties:{properties[PlayerLocation]}");
         }
     }
 
@@ -99,20 +117,22 @@ public class Launcher : MonoBehaviourPunCallbacks
             var properties = player.CustomProperties;
             Debug.Log(properties);
             Vector3 location = new Vector3();
+            Quaternion rotation = new Quaternion();
 
             properties.TryGetValue(PlayerLocation, out location);
+            properties.TryGetValue(PlayerRotation, out rotation);
 
-            Debug.Log($"player detail:{player}, location:{location}");
+            Debug.Log($"player detail:{player}, location:{location}, rotation:{rotation}");
 
-            playerData[player.UserId] = createPlayer(playerID: player.UserId, location: location);
+            playerData[player.UserId] = createPlayer(playerID: player.UserId, location: location, rotation:rotation);
 
         }
     }
 
-    private GameObject createPlayer(string playerID, Vector3 location)
+    private GameObject createPlayer(string playerID, Vector3 location, Quaternion rotation)
     {
         Debug.Log($"playerID:{playerID}, location:{location}");
-        var obj = Instantiate(playerObj, location, new Quaternion());
+        var obj = Instantiate(playerObj, location, rotation);
         var playerScript = obj.GetComponent<Player>();
 
         if (playerScript != null)
@@ -133,11 +153,14 @@ public class Launcher : MonoBehaviourPunCallbacks
             Debug.Log($"OnPlayerPropertiesUpdate player:{targetPlayer.UserId}");
 
             Vector3 location = new Vector3();
+            Quaternion rotation = new Quaternion();
+
             changedProps.TryGetValue(PlayerLocation, out location);
+            changedProps.TryGetValue(PlayerRotation, out rotation);
 
             var playerObj = playerData[targetPlayer.UserId];
             //playerObj.transform.Translate(location);
-            playerObj.transform.SetPositionAndRotation(location, new Quaternion());
+            playerObj.transform.SetPositionAndRotation(location, rotation);
 
             Debug.Log(changedProps);
 
@@ -149,12 +172,14 @@ public class Launcher : MonoBehaviourPunCallbacks
     {
         base.OnPlayerEnteredRoom(newPlayer);
 
-
         var properties = newPlayer.CustomProperties;
         Vector3 location = new Vector3();
-        properties.TryGetValue(PlayerLocation, out location);
+        Quaternion rotation = new Quaternion();
 
-        playerData[newPlayer.UserId] = createPlayer(playerID: newPlayer.UserId, location: location);
+        properties.TryGetValue(PlayerLocation, out location);
+        properties.TryGetValue(PlayerLocation, out rotation);
+
+        playerData[newPlayer.UserId] = createPlayer(playerID: newPlayer.UserId, location: location, rotation:rotation);
     }
 
     public override void OnPlayerLeftRoom(Photon.Realtime.Player otherPlayer)
