1e82bd05e89e8db4f5cce9dcba648474e5159558
Caden Tang <caden.ks.tang@gtomato.com>
2024-01-09 17:14:03 +0800

    Fix: correct spawn play position.

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -33,12 +33,17 @@ public class Launcher : MonoBehaviourPunCallbacks
             float x = Input.GetAxis("Horizontal") * 10f * Time.deltaTime;
             float z = Input.GetAxis("Vertical") * 10f * Time.deltaTime;
             var newLocation = new Vector3(x, 0, z);
+
+
             var localPlayer = playerData[PhotonNetwork.LocalPlayer.UserId];
             localPlayer.transform.Translate(newLocation);
 
             var properties = PhotonNetwork.LocalPlayer.CustomProperties;
-            properties[PlayerLocation] = newLocation;
+            properties[PlayerLocation] = localPlayer.transform.position;
+
             PhotonNetwork.LocalPlayer.SetCustomProperties(properties);
+
+            Debug.Log($"Player:{PhotonNetwork.LocalPlayer.UserId}, location:{localPlayer.transform.position}, properties:{properties[PlayerLocation]}");
         }
     }
 
@@ -52,37 +57,61 @@ public class Launcher : MonoBehaviourPunCallbacks
         PhotonNetwork.JoinOrCreateRoom("RacingRoom", options, TypedLobby.Default);
     }
 
-    public override void OnJoinedRoom()
-    {
-        base.OnJoinedRoom();
-        Debug.Log("OnJoinedRoom");
-        generateGameObj();
-    }
-
     public override void OnCreatedRoom()
     {
         base.OnCreatedRoom();
         Debug.Log("OnCreatedRoom");
     }
 
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        base.OnCreateRoomFailed(returnCode, message);
+        Debug.Log("OnCreatedRoom failed");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        Debug.Log("OnJoinedRoom");
+
+        generateGameObj();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        base.OnJoinRandomFailed(returnCode, message);
+        Debug.Log("OnJoinRandomFailed failed");
+    }
+
+    
+
     private void generateGameObj()
     {
-        playerData.Clear();
+        //playerData.Clear();
         var playerList = PhotonNetwork.PlayerList;
+
+        Debug.Log("Player count: " + playerList.Length);
+
         foreach (var player in playerList)
         {
+            Debug.Log("Player ID: " + player.ActorNumber);
+
             var properties = player.CustomProperties;
+            Debug.Log(properties);
             Vector3 location = new Vector3();
+
             properties.TryGetValue(PlayerLocation, out location);
 
-            playerData[player.UserId] = createPlayer(playerID: player.UserId, location: location);
+            Debug.Log($"player detail:{player}, location:{location}");
 
+            playerData[player.UserId] = createPlayer(playerID: player.UserId, location: location);
 
         }
     }
 
     private GameObject createPlayer(string playerID, Vector3 location)
     {
+        Debug.Log($"playerID:{playerID}, location:{location}");
         var obj = Instantiate(playerObj, location, new Quaternion());
         var playerScript = obj.GetComponent<Player>();
 
@@ -95,22 +124,31 @@ public class Launcher : MonoBehaviourPunCallbacks
 
     public override void OnPlayerPropertiesUpdate(Photon.Realtime.Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
     {
+
         base.OnPlayerPropertiesUpdate(targetPlayer, changedProps);
 
         if (targetPlayer.UserId != PhotonNetwork.LocalPlayer.UserId
             && playerData.ContainsKey(targetPlayer.UserId))
         {
+            Debug.Log($"OnPlayerPropertiesUpdate player:{targetPlayer.UserId}");
+
             Vector3 location = new Vector3();
             changedProps.TryGetValue(PlayerLocation, out location);
 
             var playerObj = playerData[targetPlayer.UserId];
-            playerObj.transform.Translate(location);
+            //playerObj.transform.Translate(location);
+            playerObj.transform.SetPositionAndRotation(location, new Quaternion());
+
+            Debug.Log(changedProps);
+
+            
         }
     }
 
     public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer)
     {
         base.OnPlayerEnteredRoom(newPlayer);
+
 
         var properties = newPlayer.CustomProperties;
         Vector3 location = new Vector3();
